d70459a14977174499abc045068224d3dfffde53
Karl Harnagy <kharnagy@inedo.com>
2016-07-15 11:29:05 -0400

    Initial commit.

---
+++ Linux/Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Linux")]
+[assembly: AssemblyDescription("Contains operations to interact with Linux servers.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2016")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
