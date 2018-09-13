de4cbd6da809f4eadf6da1cbb198c298bf4c6f98
Ben Lubar <ben.lubar@gmail.com>
2018-09-13 12:39:45 -0500

    Remove RaftStream. Use DisposingStream instead.

--- Linux/InedoExtension/Operations/SHUtil.cs
+++ Linux/InedoExtension/Operations/SHUtil.cs
@@ -7,6 +7,7 @@ using Inedo.Diagnostics;
 using Inedo.ExecutionEngine;
 using Inedo.Extensibility.Operations;
 using Inedo.Extensibility.RaftRepositories;
+using Inedo.IO;
 
 namespace Inedo.Extensions.Linux.Operations
 {
@@ -64,7 +65,7 @@ namespace Inedo.Extensions.Linux.Operations
                     logger.LogDebug("Executing script...");
                 }
 
-                var ps = context.Agent.GetService<IRemoteProcessExecuter>();
+                var ps = await context.Agent.GetServiceAsync<IRemoteProcessExecuter>().ConfigureAwait(false);
                 int? exitCode;
 
                 using (var process = ps.CreateProcess(new RemoteProcessStartInfo { FileName = fileName, WorkingDirectory = context.WorkingDirectory, Arguments = arguments }))
@@ -125,63 +126,14 @@ namespace Inedo.Extensions.Linux.Operations
                     return null;
                 }
 
-                return new StreamReader(new RaftStream(await raft.OpenRaftItemAsync(RaftItemType.Script, scriptName, FileMode.Open, FileAccess.Read), raft), InedoLib.UTF8Encoding);
+                var item = await raft.OpenRaftItemAsync(RaftItemType.Script, scriptName, FileMode.Open, FileAccess.Read);
+                return new StreamReader(new DisposingStream(item, item, raft), InedoLib.UTF8Encoding);
             }
             catch
             {
                 try { raft?.Dispose(); }
                 catch { }
                 throw;
-            }
-        }
-
-        private sealed class RaftStream : Stream
-        {
-            private Stream baseStream;
-            private RaftRepository raft;
-            private bool disposed;
-
-            public RaftStream(Stream baseStream, RaftRepository raft)
-            {
-                this.baseStream = baseStream;
-                this.raft = raft;
-            }
-
-            public override bool CanRead => this.baseStream.CanRead;
-            public override bool CanSeek => this.baseStream.CanSeek;
-            public override bool CanWrite => this.baseStream.CanWrite;
-            public override long Length => this.baseStream.Length;
-            public override long Position
-            {
-                get => this.baseStream.Position;
-                set => this.baseStream.Position = value;
-            }
-
-            public override void Flush() => this.baseStream.Flush();
-            public override int Read(byte[] buffer, int offset, int count) => this.baseStream.Read(buffer, offset, count);
-            public override long Seek(long offset, SeekOrigin origin) => this.baseStream.Seek(offset, origin);
-            public override void SetLength(long value) => this.baseStream.SetLength(value);
-            public override void Write(byte[] buffer, int offset, int count) => this.baseStream.Write(buffer, offset, count);
-            public override int ReadByte() => this.baseStream.ReadByte();
-            public override void WriteByte(byte value) => this.baseStream.WriteByte(value);
-            public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => this.baseStream.ReadAsync(buffer, offset, count, cancellationToken);
-            public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) => this.baseStream.WriteAsync(buffer, offset, count, cancellationToken);
-            public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken) => this.baseStream.CopyToAsync(destination, bufferSize, cancellationToken);
-
-            protected override void Dispose(bool disposing)
-            {
-                if (!this.disposed)
-                {
-                    if (disposing)
-                    {
-                        this.baseStream.Dispose();
-                        this.raft.Dispose();
-                    }
-
-                    this.disposed = true;
-                }
-
-                base.Dispose(disposing);
             }
         }
     }
