ddcc4500885994ca4db4557025ee3b0b66ec1953
Greg D <gdivis@inedo.com>
2019-12-13 12:51:01 -0500

    added AppliesToAttribute

--- Linux/InedoExtension/Properties/AssemblyInfo.cs
+++ Linux/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Inedo.Extensibility;
 
 [assembly: AssemblyTitle("Linux")]
 [assembly: AssemblyDescription("Contains operations to interact with Linux servers.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2019")]
 [assembly: AssemblyVersion("1.0.3")]
 [assembly: AssemblyFileVersion("1.0.3")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
+
+[assembly: AppliesToAttribute(InedoProduct.BuildMaster | InedoProduct.Otter)]
