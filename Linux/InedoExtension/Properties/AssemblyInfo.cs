43c0ab137122fa9cb7e74de9cfd0951c28227a86
gdivis <gdivis@inedo.com>
2024-10-23 23:18:16 -0400

    Sdk 3.1 (#15)
    * update to sdk 3.1
    
    * update sdk

--- Linux/InedoExtension/Properties/AssemblyInfo.cs
+++ Linux/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("Linux")]
 [assembly: AssemblyDescription("Contains operations to interact with Linux servers.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
 
